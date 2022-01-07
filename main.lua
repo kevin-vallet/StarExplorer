e4d19ca5cca86ea8f4c794ba99c4cf7e1f5af990
kevin-vallet <kevin.vallet@gmail.com>
2022-01-07 21:25:37 +0100

    add collision, restore ship and gameLoop

--- main.lua
+++ main.lua
@@ -54,11 +54,11 @@ local lives = 3;
 local score = 0;
 local died = false;
 
-local asteroidTable = {};
+local asteroidsTable = {};
 
 local ship;
 local gameLoopTimer;
-local liveText;
+local livesText;
 local scoreText;
 
 -- Set up display groups
@@ -78,20 +78,20 @@ physics.addBody(ship ,{ radius=30, isSensor = true });
 ship.myName = "ship";
 
 -- Display lives and score
-liveText = display.newText( uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36);
+livesText = display.newText( uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36);
 scoreText = display.newText( uiGroup, "Score: " .. score, 400, 80, native.systemFont, 36);
 
 -- Hide the status bar
 display.setStatusBar( display.HiddenStatusBar );
 
 local function updateText()
-    liveText.text = "Lives: " .. lives;
+    livesText.text = "Lives: " .. lives;
     scoreText.text = "Scores " .. score;
 end
 
 local function createAsteroid()
     local newAsteroid = display.newImageRect( mainGroup, objectSheet, 1, 102, 85);
-    table.insert(asteroidTabke, newAsteroid);
+    table.insert(asteroidsTable, newAsteroid);
     physics.addBody(newAsteroid, "dynamic", {radius=40, bounce=0.8});
     newAsteroid.myName = "asteroid";
 
@@ -155,3 +155,87 @@ local function dragShip(event)
 end
 
 ship:addEventListener("touch", dragShip)
+
+
+
+local function gameLoop()
+    -- Create new asteroid
+    createAsteroid();
+    
+    -- Remove asteroids wich have drifted off screen
+    for i = #asteroidsTable, 1, -1 do
+        local thisAsteroid = asteroidsTable[i];
+ 
+        if (thisAsteroid.x < -100 or
+            thisAsteroid.x > display.contentWidth + 100 or
+            thisAsteroid.y < -100 or
+            thisAsteroid.y > display.contentHeight + 100)
+        then
+            display.remove(thisAsteroid);
+            table.remove(asteroidsTable, i);
+        end
+    end
+
+end
+
+gameLoopTimer = timer.performWithDelay(500, gameLoop, 0);
+
+
+local function restoreShip()
+    ship.isBodyActive = false;
+    ship.x = display.contentCenterX;
+    ship.y = display.contentHeight - 100;
+
+    -- Fade in the ship
+    transition.to(ship, {alpha = 1, time = 4000, 
+        onComplete = function ()
+            ship.isBodyActive = true;
+            died = false;
+        end
+    });
+end
+
+local function onCollision(event)
+    if (event.phase == "began") then
+        local obj1 = event.object1;
+        local obj2 = event.object2;
+
+        if ((obj1.myName == "laser" and obj2.myName == "asteroid") or
+            (obj1.myName == "asteroid" and obj2.myName == "laser"))
+        then
+            display.remove(obj1);
+            display.remove(obj2);
+
+            for i = #asteroidsTable, 1, -1 do
+                if (asteroidsTable[i] == obj1 or asteroidsTable[i] == obj2) then
+                    table.remove( asteroidsTable, i);
+                    break;
+                end
+            end
+
+            -- Increase score
+            score = score + 100;
+            scoreText.text = "Score: " .. score;
+
+        elseif ((obj1.myName == "ship" and obj2.myName == "asteroid") or
+               (obj1.myName == "asteroid" and obj2.myName == "ship"))
+        then
+            if (died == false) then
+                died = true;
+
+                -- Update lives
+                lives = lives - 1;
+                livesText.text = "Lives: " .. lives;
+
+                if (lives == 0) then
+                    display.remove(ship);
+                else
+                    ship.alpha = 0;
+                    timer.performWithDelay(1000,restoreShip);
+                end
+            end
+        end
+    end
+end
+
+Runtime:addEventListener("collision", onCollision)
